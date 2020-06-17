f6739ec772ef122b699481611081feddc779607f
Md Abdul Kadir <maktareq@gmail.com>
2020-06-18 01:31:05 +0200

    removed from seetings file

--- violance_analytics/violance_analytics/settings.py
+++ violance_analytics/violance_analytics/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'violance_analytics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], # leting django know where is the template folder
+        'DIRS': [], # leting django know where is the template folder os.path.join(BASE_DIR, 'templates')
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
