a1cbc52ec68b5eb54f93b91fe8837543309aeae1
Md Abdul Kadir <maktareq@gmail.com>
2020-06-17 09:28:53 +0200

    static root error

--- violance_analytics/violance_analytics/settings.py
+++ violance_analytics/violance_analytics/settings.py
@@ -124,7 +124,12 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # This should already be in your settings.py
 django_heroku.settings(locals())
 # This is new
