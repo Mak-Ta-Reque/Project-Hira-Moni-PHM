52a66e78a53faac7640445723900bba8c45c99a1
Md Abdul Kadir <maktareq@gmail.com>
2020-06-17 10:54:30 +0200

    base directory name

--- violance_analytics/violance_analytics/settings.py
+++ violance_analytics/violance_analytics/settings.py
@@ -16,8 +16,7 @@ import dotenv
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.join(BASE_DIR, '..')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # This is new:
 
 import dotenv
