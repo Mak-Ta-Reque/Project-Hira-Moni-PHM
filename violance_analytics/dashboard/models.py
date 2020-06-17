9f7583b67242c036b8d19c701c8211067c671474
Md Abdul Kadir <maktareq@gmail.com>
2020-06-17 19:31:58 +0200

    redundent code from model remove

--- violance_analytics/dashboard/models.py
+++ violance_analytics/dashboard/models.py
@@ -1,33 +1,5 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
-class Order(models.Model):
-    product_category = models.CharField(max_length=20)
-    payment_method = models.CharField(max_length=50)
-    shipping_cost = models.CharField(max_length=50)
-    unit_price = models.DecimalField(max_digits=5, decimal_places=2)
-
-class Country(models.Model):
-    name = models.CharField(max_length=30)
-
-class City(models.Model):
-    name = models.CharField(max_length=30)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    population = models.PositiveIntegerField()
-
-class ToDoList(models.Model):
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
-class Item(models.Model):
-    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
-    text = models.CharField(max_length=200)
-    completed = models.BooleanField()
-
-    def __str__(self):
-        self.text
-
 
 
