ccc521bdbe4cb77dfce3df08cfc9f4df3b62adec
Md Abdul Kadir <maktareq@gmail.com>
2020-06-17 13:05:10 +0200

    project banner is created

--- violance_analytics/dashboard/urls.py
+++ violance_analytics/dashboard/urls.py
@@ -3,8 +3,8 @@ from . import views, admin
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('population-chart/', views.population_chart, name='population-chart'),
-    path('pie-chart/', views.pie_chart, name='pie-chart'),
-    path("create/", views.create, name="create"),
-    path("<int:id>", views.index, name="create"),
+    #path('population-chart/', views.population_chart, name='population-chart'),
+    #path('pie-chart/', views.pie_chart, name='pie-chart'),
+    #path("create/", views.create, name="create"),
+    #path("<int:id>", views.index, name="create"),
 ]
