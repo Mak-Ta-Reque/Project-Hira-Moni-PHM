f0b2dd8dcb7052b61ac66b6d0d649797d75589b1
Md Abdul Kadir <maktareq@gmail.com>
2020-06-17 10:05:13 +0200

    .keep

--- violance_analytics/violance_analytics/settings.py
+++ violance_analytics/violance_analytics/settings.py
@@ -140,12 +140,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
