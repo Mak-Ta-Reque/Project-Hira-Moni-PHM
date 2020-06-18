0454ec9eb14ee04d29d23d3035b15759ae400753
Md Abdul Kadir <maktareq@gmail.com>
2020-06-18 11:38:54 +0200

    no problem is local sevrer

--- violance_analytics/dashboard/models.py
+++ violance_analytics/dashboard/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=200)
-    violance = models.IntegerField()
+    violence = models.IntegerField()
 
     def __str__(self):
-        return "{}-{}".format(self.name, self.violance)
+        return "{}-{}".format(self.name, self.violence)
