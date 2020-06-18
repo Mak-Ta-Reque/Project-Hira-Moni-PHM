6b9a742d57dee827f5c81b9e020b57c95f288709
Md Abdul Kadir <maktareq@gmail.com>
2020-06-18 11:58:37 +0200

    fresh data

--- violance_analytics/dashboard/admin.py
+++ violance_analytics/dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-#from .models import City
+from .models import City
 # Register your models here.
 
-#admin.site.register(City)
+admin.site.register(City)
--- violance_analytics/dashboard/views.py
+++ violance_analytics/dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-#from .models import City
+from .models import City
 
 def home(request):
     return render(request, 'index.html')
@@ -11,6 +11,6 @@ class CityCharView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        #context["qs"] = City.objects.all()
+        context["qs"] = City.objects.all()
         return context
 
