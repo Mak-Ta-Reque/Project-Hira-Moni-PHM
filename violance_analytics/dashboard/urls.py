c2622abca4b1fa89c5c8a65dcf31d7b8f415a8c0
Md Abdul Kadir <maktareq@gmail.com>
2020-06-28 16:09:18 +0200

    redundent file removed

--- violance_analytics/dashboard/urls.py
+++ violance_analytics/dashboard/urls.py
@@ -6,9 +6,6 @@ from .views import CityCharView, ListCity
 
 urlpatterns = [
     path('', views.home),
-    path('customer', views.customer),
-    path('product', views.products),
     path('chart', CityCharView.as_view(), name='chart'),
-    path('list', ListCity.as_view(),name='list')
 
 ]
