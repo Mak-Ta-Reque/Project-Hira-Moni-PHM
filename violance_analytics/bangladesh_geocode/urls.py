dd818738ce79b55b54e435305f2c0967cdddc0b6
Md Abdul Kadir <maktareq@gmail.com>
2020-07-26 23:51:06 +0200

    migration deleted

--- violance_analytics/bangladesh_geocode/urls.py
+++ violance_analytics/bangladesh_geocode/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
+
 from . import views, db_manager
 
 urlpatterns = [
-    
+    path('', views.initialize, name='init'),
 ]
 
 
 
-db_manager.populate_database()
+#db_manager.populate_database()
--- violance_analytics/bangladesh_geocode/views.py
+++ violance_analytics/bangladesh_geocode/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .db_manager import populate_database
+
 
 # Create your views here.
+def initialize(request):
+    populate_database()
+
+    return HttpResponse(status=201)
